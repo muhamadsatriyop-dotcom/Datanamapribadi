b1f7a13d42efc9c345404f09782e86c3eab79994
muhamadsatriyop-dotcom <muhamadsatriyop@gmail.com>
2025-10-07 12:01:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const tombol sapa = document.getElementByld('sapaButton'):
+const tombol sapa = document.getElementByld('Sapa AKUUUU!'):
 tombol Sapa.addEventListerner('click',function() {
   alert('SUWON WES RENE,):}):
